38238f983461289629577a1f150151a7fe1173dd
fifuyaya <mardhiyahnura@student.uns.ac.id>
2023-03-14 14:50:29 +0700

    menambahkan file exercise-01

--- Exercise-01.py
+++ Exercise-01.py
@@ -1,5 +1,5 @@
 # Exercise-01
-print(" ")
+print("===================================")
 nama = "Mardhiyah Nur Afiahni"
 nim = "I0322074"
 
@@ -8,3 +8,4 @@ print("NIM  : ", nim)
 print("Rumah tempat tinggalku berada di Kota Semarang")
 print("Kota Semarang merupakan ibu kota Provinsi Jawa Tengah yang suhu udara pada siang harinya selalu lebih dari 30 derajat celcius, memiliki monumen Tugu Muda yang berada di tengah kota, serta jalanan yang selalu padat dengan kendaraan pada saat jam berangkat dan pulang kantor")
 print("Harapanku sebagai lulusan Teknik Industri adalah bisa memakai ilmu yang didapat untuk hal yang bermanfaat dan untuk menghasilkan uang tanpa bantuan orang tua")
+print("===================================")
